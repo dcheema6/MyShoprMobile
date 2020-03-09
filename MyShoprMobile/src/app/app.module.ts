09a01fa5d538644c2041b9b04c559d996a76283f
Matt Northmore <rmnorthmore@gmail.com>
2020-03-09 15:51:14 -0400

    added login, updated user model, service, other module cleanup, package.json changes and readme updates

--- MyShoprMobile/src/app/app.module.ts
+++ MyShoprMobile/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { UserService } from "./services/user.service";
 
 @NgModule({
     bootstrap: [
@@ -17,8 +18,12 @@ import { AppComponent } from "./app.component";
     declarations: [
         AppComponent
     ],
+    providers: [
+        UserService
+    ],
     schemas: [
-        NO_ERRORS_SCHEMA
+        NO_ERRORS_SCHEMA,
+        CUSTOM_ELEMENTS_SCHEMA
     ]
 })
 export class AppModule { }
--- MyShoprMobile/src/app/models/user.model.ts
+++ MyShoprMobile/src/app/models/user.model.ts
@@ -3,4 +3,12 @@ export class User {
     email: String;
     password: String;
     isVerified?: Boolean;
+    permissionRole: APP_ROLES;
+}
+
+export enum APP_ROLES {
+    Admin = 'Admin',
+    Manager = 'Manager',
+    User = 'User',
+    Visitor = 'Visitor'
 }
