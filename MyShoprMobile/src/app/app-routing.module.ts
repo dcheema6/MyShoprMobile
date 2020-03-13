561a6ed115b429d5751036765a7e7d65ccf124f4
Marcus Chien <mchien5@uwo.ca>
2020-03-12 21:14:44 -0400

    Native Script Drop Down added

--- MyShoprMobile/src/app/app-routing.module.ts
+++ MyShoprMobile/src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
     { path: "new-recipe", loadChildren: () => import("~/app/create-recipe/create-recipe.module").then((m) => m.CreateRecipeModule) },
     { path: "store-picker", loadChildren: () => import("~/app/store-picker/store-picker.module").then((m) => m.StorePickerModule) },
     { path: "goshop", loadChildren: () => import("~/app/go-shopping/go-shopping.module").then((m) => m.GoShoppingModule) },
-    { path: "login", loadChildren: () => import("~/app/login/login.module").then((m) => m.LoginModule) }
+    { path: "login", loadChildren: () => import("~/app/login/login.module").then((m) => m.LoginModule) },
+    { path: "dashboard", loadChildren: () => import("~/app/dashboard/dashboard.module").then((m) => m.DashboardModule) }
 ];
 
 @NgModule({
