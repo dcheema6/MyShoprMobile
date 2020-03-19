1be1d90aebbb9f5ed33783897026be78f4c30d50
dcheema6 <dcheema6@uwo.ca>
2020-03-18 22:01:23 -0400

    shopping title bug fix

--- MyShoprMobile/src/app/app-routing.module.ts
+++ MyShoprMobile/src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+    { path: "", redirectTo: "/login", pathMatch: "full" },
     { path: "home", loadChildren: () => import("~/app/home/home.module").then((m) => m.HomeModule) },
-    { path: "new-recipe", loadChildren: () => import("~/app/create-recipe/create-recipe.module").then((m) => m.CreateRecipeModule) },
     { path: "store-picker/:id", loadChildren: () => import("~/app/store-picker/store-picker.module").then((m) => m.StorePickerModule) },
     { path: "goshop", loadChildren: () => import("~/app/go-shopping/go-shopping.module").then((m) => m.GoShoppingModule) },
     { path: "login", loadChildren: () => import("~/app/login/login.module").then((m) => m.LoginModule) },
